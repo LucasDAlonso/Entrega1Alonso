72982cb40f77fd2886f3269b1024b1ea64120d6c
LucasDAlonso <lucas.lda93@gmail.com>
2022-07-06 10:34:17 -0300

    3rd commit

--- CrearBlog/urls.py
+++ CrearBlog/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import vista1, crear_blog 
+from .views import vista1, crear_blog, acerca
 
 urlpatterns = [
     path('', vista1,name="home"),
     path('crear-blog/',crear_blog,name="crear_blog"),
+    # path('aboutus/',acerca,name = "aboutus")
 
 ]
